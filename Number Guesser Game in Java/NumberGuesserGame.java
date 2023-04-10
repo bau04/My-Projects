522f9cfbd971373a1103efad3383466863dab66c
bau04 <120693660+bau04@users.noreply.github.com>
2023-04-10 20:38:11 +0800

    Update NumberGuesserGame.java

--- Number Guesser Game in Java/NumberGuesserGame.java
+++ Number Guesser Game in Java/NumberGuesserGame.java
@@ -9,25 +9,27 @@ public class App {
     Scanner sc = new Scanner(System.in);
 
     System.out.println("Number Guesser Game!");
-    System.out.println("Guess a number between 1 and 3");
+    System.out.println("Guess a number between 1 and 6");
     System.out.println("Type your number: ");
     int n = sc.nextInt();
 
     Random random = new Random();
 
     //Generates random number and the limits of it
-    int r = random.nextInt(2,4);
+    int r = random.nextInt(1,6);
 
 
     if(r == n) {
         System.out.println("Your guess is correct!");
+        System.out.println("Your answer: " + n)
+        System.out.println("Correct Answer: " + r)
     }
 
     else if(n < 1) {
         System.out.println("Your answer is below the range");
     }
 
-    else if(n > 3) {
+    else if(n > 6) {
         System.out.println("Your answer is above the range");
     }
 
