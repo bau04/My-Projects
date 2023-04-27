4b4926934942c4ee69c187bd495e114f52c7ed38
bau04 <120693660+bau04@users.noreply.github.com>
2023-04-28 00:46:32 +0800

    Create Shape.java

---
+++ Problem Assignments/Problem 1 (shape)/Shape.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Shape {
+
+    double sBase, sHeight, area;
+
+    public void calculateArea(double sHeight, double sBase){
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter base measurement: ");
+        this.sBase = sc.nextDouble();
+
+        System.out.println("Enter height measurement: ");
+        this.sHeight = sc.nextDouble();
+
+        double area = sBase * sHeight;
+        
+        area = new area.calculateArea();
+
+
+
+    }
+    public static void main(String[] args) {
+
+        }
+    }
