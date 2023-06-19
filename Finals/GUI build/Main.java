c33d95f2a1b9a699ecb13bc01d0b9951b7237adc
bau04 <120693660+bau04@users.noreply.github.com>
2023-06-19 12:34:01 +0800

    Create Main.java

---
+++ Finals/GUI build/Main.java
@@ -0,0 +1,14 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        LaunchPage launchPage = new LaunchPage();
+
+    }
+}
